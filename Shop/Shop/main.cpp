84d39fa65f2f2ba2b43ce3dfe4b6117d35a2364b
BulaniiSergey <91825050+BylaniiSergey@users.noreply.github.com>
2024-11-22 14:43:40 +0300

    Удаление

--- Shop/Shop/main.cpp
+++ Shop/Shop/main.cpp
@@ -24,35 +24,40 @@ int main()
 
     // Создание продуктов
     auto product1 = std::make_shared<Product>("001", "Хлеб", 50.0);
-    auto product2 = std::make_shared<Product>("002", "Булочка", 25.0);
-    auto product3 = std::make_shared<Product>("003", "Круассан", 30.0);
-    std::cout << "Продукты созданы!" << std::endl;
+    auto product2 = std::make_shared<Product>("002", "Телефон", 25000.0);
+    auto product3 = std::make_shared<Product>("003", "Яйца", 130.0);
 
     // Добавление продуктов на склад
     warehouse.AddProduct(product1);
     warehouse.AddProduct(product2);
     warehouse.AddProduct(product3);
 
+
     // Вывод информации о всех товарах на складе
     warehouse.PrintInventory();
 
     // Создание магазина
-    bakery::shop::Store store(123456789, "Магазин выпечки", "Улица Пекарей, 1");
-    std::cout << store.toString() << std::endl;
+    bakery::shop::Store store1(123456789, "Магазин выпечки", "Улица Пекарей д.1");
+    bakery::shop::Store store2(987654321, "Магазин электронники", "Пушкинская улица д.14");
+    bakery::shop::Store store3(13580, "Магазин продуктов", "Улица Артюшина д.32");
+    std::cout << store1.toString() << std::endl;
+    std::cout << store2.toString() << std::endl;
+    std::cout << store3.toString() << std::endl;
 
     // Создание клиентов
     bakery::client::Client client1("Иванов И.И.", 10.0);
     bakery::client::Client client2("Петрова А.А.", 15.0);
+    bakery::client::Client client3("Аксютин В.В.", 5.0);
     std::cout << client1.toString() << std::endl;
     std::cout << client2.toString() << std::endl;
-    
+    std::cout << client3.toString() << std::endl;
 
     // Создание накладной
-    bakery::invoice::Invoice invoice(store, client1, "INV001", "2023-10-01");
+    bakery::invoice::Invoice invoice(store1, client1, "INV001", "2024-11-22");
 
     // Добавление товаров в накладную
     invoice.addProduct(product1);
-    invoice.addProduct(product2);
+    invoice.addProduct(product3);
 
     // Вывод наклодной
     std::cout << invoice.toString() << std::endl;
